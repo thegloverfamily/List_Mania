e534e8f4e2507d541c152af464c4d702596b482d
thegloverfamily <jon@thegloverfamily.co.uk>
2016-11-04 10:37:22 +0000

    Initial Development
    Adding in functionality for task lists.

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+
+
+
+function displayList()
+{
+    //Display Dynamic HTML Lists
+    //UL and Li Tags
+    //Remove Button Displays
+}
+function addTask()
+{
+    //Add in the tasks from the input field in our HTML file
+    //Display the latest list of tasks
+    displayList();
+}
+
+
+displayList();
+document.getElementById("Add").addEventListener("click", addTask);
