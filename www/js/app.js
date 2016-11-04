9259c1548cda061210cfc6f8f80fc3f2e35f6a1b
thegloverfamily <jon@thegloverfamily.co.uk>
2016-11-04 13:02:11 +0000

    Complete Functionality with No Memory
    Remove button now working
    Rebranded with Christ Church colours and theme.

--- www/js/app.js
+++ www/js/app.js
@@ -13,11 +13,17 @@ function displayList()
     //Create LI Tags and loop through the Array
     for(var i=0; i < taskList.length; i++)
     {
-        taskListContent += "<li>"+taskList[i]+"<button>Remove Item</button></li>";
+        taskListContent += "<li>"+taskList[i]+"<button class='remove' id='"+i+"'>Remove Item</button></li>";
     }
     taskListContent += "</ul>";
     //Replace the HTML Elements inside of the id="TaskListValue"
     document.getElementById("TaskListValue").innerHTML = taskListContent;
+    
+    var btnArray = document.getElementsByClassName("remove");
+    for(i = 0; i <btnArray.length; i++)
+    {
+        btnArray[i].addEventListener('click', removeTask);
+    }
 }
 
     //Add in the tasks from the input field in our HTML file
@@ -36,6 +42,9 @@ function addTask()
 function removeTask()
 {
     //Remove Tasks from the Array
+    var rID = this.getAttribute("id");
+    taskList.splice(rID, 1);
+    displayList();
 }
 
 displayList();
